a61cd1dd664cee57f747bcb3803ce896bb8d8455
Todd H <github@simplybest.com>
2017-08-16 17:10:53 -0600

    your firebase config and google api key go in here

---
+++ src/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "123",
+    authDomain: "123.firebaseapp.com",
+    databaseURL: "https://123.firebaseio.com",
+    projectId: "123",
+    storageBucket: "123.appspot.com",
+    messagingSenderId: "123"
+  },
+    googleCloudVisionAPIKey: "123"
+};
